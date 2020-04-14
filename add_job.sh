0a575c7cea6576fa655c1b34268422fb7c3b088e
superorc <rykanoga@gmail.com>
2020-04-14 22:05:12 +0300

    copy sample job to cli container

--- add_job.sh
+++ add_job.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+JENKINS_URL='http://jenkins-master:8080'
+JENKINS_USER='a_user'
+JENKINS_PWD='a_pwd'
+
 # get cli
 wget http://jenkins-master:8080/jnlpJars/jenkins-cli.jar
 
+# coomand
+java -jar jenkins-cli.jar $JENKINS_URL -auth $JENKINS_USER:$JENKINS_PWD --help
