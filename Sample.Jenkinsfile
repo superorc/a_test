e25837d1d818750c2ca0429322e8a9710978863f
superorc <rykanoga@gmail.com>
2020-04-15 01:15:30 +0300

    add job from a_pipeline_job.xml

--- Sample.Jenkinsfile
+++ Sample.Jenkinsfile
@@ -2,10 +2,15 @@
 // a_test job to check dind working
 node () {
 
-    stage("Test docker") {
+    stage("Test dockerd is avaliable") {
         docker.image('alpine:latest').inside() {
-            sh 'uname -a'
+            sh 'echo "poc for A, hey Inna"'
         }
+    }
+
+    stage("Get src form king's repo") {
+        git branch: '*/job', 
+        url: 'https://github.com/superorc/a_test.git'
     }
 
 }
--- add_job.sh
+++ add_job.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+set -e
+
 JENKINS_URL='http://jenkins-master:8080'
 JENKINS_USER='a_user'
 JENKINS_PWD='a_pwd'
 
-# get cli
-wget http://jenkins-master:8080/jnlpJars/jenkins-cli.jar
+# get cli from master
+wget http://jenkins-master:8080/jnlpJars/jenkins-cli.jar --output-document=jenkins-cli.jar
 
-# coomand
-java -jar jenkins-cli.jar $JENKINS_URL -auth $JENKINS_USER:$JENKINS_PWD --help
+# command
+echo "injecting job config"
+#echo "/root/.jenkins/jobs/a_pipeline/config.xml" > java -jar jenkins-cli.jar -s $JENKINS_URL -auth $JENKINS_USER:$JENKINS_PWD create-job a_pipeline_poc
+java -jar jenkins-cli.jar -s $JENKINS_URL -auth $JENKINS_USER:$JENKINS_PWD create-job a_pipeline_poc < /root/.jenkins/jobs/a_pipeline/config.xml
+
+
+# restart master
+echo "restarting master"
+java -jar jenkins-cli.jar -s $JENKINS_URL -auth $JENKINS_USER:$JENKINS_PWD restart
